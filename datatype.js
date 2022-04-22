8ee1d83670e94dd0157634721747957a5a07732b
programmermohaimin <you@example.com>
2022-04-22 16:51:43 +0600

    datatype

---
+++ datatype.js
@@ -0,0 +1,10 @@
+
+
+let Age = 20;
+let result= Age%2 === 0 ? 'true':'even';
+console.log(result);
+
+//here is ternary operator in the bottom
+
+let names = Age %2 === 0 ? 'true' : 'false'
+console.log(names);
